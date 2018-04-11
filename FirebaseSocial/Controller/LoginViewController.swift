da17f68036a20debb2ebc8c16978b7ff5b23641e
Ryan <rcarnovsky@gmail.com>
2018-04-11 12:55:06 -0400

    Add email login method with Firebase

--- FirebaseSocial/Controller/LoginViewController.swift
+++ FirebaseSocial/Controller/LoginViewController.swift
@@ -1,18 +1,38 @@
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var txtEmail: CustomUITextField!
+    @IBOutlet weak var txtPassword: CustomUITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        txtPassword.isSecureTextEntry = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        if let email = txtEmail.text, let password = txtPassword.text{
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, err) in
+                if err == nil{
+                    print("FIRE: Signed in with email")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, err) in
+                        if err != nil{
+                            print("FIRE: Unable to auth with email")
+                        } else {
+                            print("FIRE: User created with email")
+                        }
+                    })
+                }
+            })
+        } else {
+            print("FIRE: Please enter an email and password.")
+        }
+    }
+    
 }
 
