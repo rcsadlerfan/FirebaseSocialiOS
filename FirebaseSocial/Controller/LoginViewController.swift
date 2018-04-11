07022c40c8ebe0c9e0726bfb8eb498650fa8356f
Ryan <rcarnovsky@gmail.com>
2018-04-11 08:37:58 -0400

    Custom view classes for LoginUI

--- FirebaseSocial/Controller/LoginViewController.swift
+++ FirebaseSocial/Controller/LoginViewController.swift
@@ -2,8 +2,11 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtPassword.isSecureTextEntry = true
     }
 
     override func didReceiveMemoryWarning() {
---
+++ FirebaseSocial/Util/Constants.swift
@@ -0,0 +1,3 @@
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
