bfe62669df4cc56b2937053749f60f1ab502af34
Ryan <rcarnovsky@gmail.com>
2018-04-10 20:39:52 -0400

    Create login UI

--- FirebaseSocial/View/LoginViewController.swift
+++ FirebaseSocial/View/LoginViewController.swift
@@ -1,23 +1,13 @@
-//
-//  ViewController.swift
-//  FirebaseSocial
-//
-//  Created by Ryan Carnovsky on 4/10/18.
-//  Copyright © 2018 Ryan Carnovsky. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
